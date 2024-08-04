f08b8d51725455c821ffd1fb342debbfe7819b27
muzanpvp <mkibutsuji4@gmail.com>
2024-08-04 12:42:18 -0300

    ..

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 const Express = require('express');
-const context = require('../db/db-context');
+const path = require('path');
+//const context = require('../db/db-context');
 
 const app= Express();
 const port = 5500;
 
 // Configurar o diretório de views e o motor de visualização
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './views/Home'));
+app.set('views', path.join(__dirname, '../views/Home'));
 
 app.get('/',(req,res)=>{
   res.render('home');
